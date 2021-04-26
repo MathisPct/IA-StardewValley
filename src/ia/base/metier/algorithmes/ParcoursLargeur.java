8570df4bef52b6863600ae5e6676ab4f50c37a14
MathisPct <math712b@gmail.com>
2021-04-26 17:27:34 +0200

    first version of class

---
+++ src/ia/base/metier/algorithmes/ParcoursLargeur.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia.base.metier.algorithmes;
+
+import ia.base.metier.carte.Carte;
+import ia.base.metier.carte.cases.Case;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mp671721
+ */
+public class ParcoursLargeur extends AlgorithmeCalculDistance{
+    /**
+     * Constructeur par initialisation
+     * @param carte que l'on veut traitée
+     */
+    public ParcoursLargeur(Carte carte) {
+        super(carte);
+    }
+
+    @Override
+    public void calculerDistancesDepuis(Case depart) {
+        Case caseEnCours;
+        
+        ArrayList<Case> aTraiter = new ArrayList<>();
+        super.reintialisationDistances();
+        aTraiter.add(depart);
+        super.setDistance(depart, 0);
+        do{
+            caseEnCours = aTraiter.get(0);
+            aTraiter.remove(aTraiter.get(0));
+            //pour tout voisin de la caseEnCours
+            for (Case v : caseEnCours.getVoisins()) {
+                if(super.getDistance(v) == null){
+                    //si les voisins sont accessibles
+                    if(v.estAccessible()){
+                        super.setDistance(v, super.getDistance(caseEnCours)+1);
+                        aTraiter.add(v);
+                    }
+                }
+            }
+        }while(aTraiter.size()>0);
+    }
+    
+}
